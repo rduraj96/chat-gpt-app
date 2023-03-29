7a1862ed8ead36bc9ea3396a6ddb49a1ba86ec69
rduraj96 <42787617+rduraj96@users.noreply.github.com>
2023-03-28 23:57:48 -0400

    Changed Login import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Chat from "@/components/chat";
-import Login from "@/components/Login";
+import Login from "@/components/login";
 
 function App() {
   const [user, setUser] = useState(null);
