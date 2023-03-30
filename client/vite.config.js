efaa24b9ef62e59438d46680e68fe5147de34ffb
rduraj96 <42787617+rduraj96@users.noreply.github.com>
2023-03-29 21:06:39 -0400

    Build fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
+import commonjs from 'vite-plugin-commonjs'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), commonjs()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    }
   }
 })
